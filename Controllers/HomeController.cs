f6cc2953c8b85985df22eaa3af6efeb3cf75e6a7
Tiah Keefe <tetered12@gmail.com>
2021-12-06 13:51:28 -0600

    Adding a basic SearchController and View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace SearchSaver.Controllers
 
         public IActionResult Index()
         {
-            return View();
+           return View();
         }
 
         public IActionResult Privacy()
---
+++ Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchSaver.Controllers
+{
+    [Route("/search")]
+    public class SearchController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Results(string product = "TV")
+        {
+            return Content("<h1>Seach results for " + product + "!<h1>", "text/html");
+        }
+    }
+}
