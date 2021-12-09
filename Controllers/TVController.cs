33a75dabb951f3413862a532a8201c5941f8c92e
Tiah Keefe <tetered12@gmail.com>
2021-12-08 21:17:52 -0600

    Adding a tv submission form

---
+++ Controllers/TVController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchSaver.Controllers
+{
+    public class TVController : Controller
+    {
+
+        static private List<string> TVS = new List<string>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.tvs = TVS;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/TV/Add")]
+        public IActionResult NewTV(string name)
+        {
+            TVS.Add(name);
+            return Redirect("/TV");
+        }
+    }
+}
